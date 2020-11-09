448cbc1e190bacc5f20d3fd9a6059ec26e429f87
Familia <araujobarbara228@gmail.com>
2020-11-09 11:06:20 -0400

    Ventana

---
+++ Practicas1/Hello.java
@@ -0,0 +1,14 @@
+package Practicas1;
+
+import Vista.Vista;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		Vista V = new Vista();
+	
+	}
+
+}
